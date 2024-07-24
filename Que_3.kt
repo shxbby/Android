e911b639ce86197535cdb0a707343d18bd41d6ec
shxbby <148192389+shxbby@users.noreply.github.com>
2024-07-24 07:46:35 +0530

    Add files via upload

---
+++ Que_3.kt
@@ -0,0 +1,11 @@
+//3. Write a program that accept two numbers and perform all basic mathematicaloperation and print.
+fun main() {
+    var num1 : Int= 20
+    var num2 = 10
+
+    println("Sum = ${num1+num2}")
+    println("Substraction = ${num1-num2}")
+    println("Multiplication = ${num1*num2}")
+    println("Divison = ${num1/num2}")
+
+}
