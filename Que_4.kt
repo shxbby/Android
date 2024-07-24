7fd3df4084f8b864f44851c45f44cf7c63c0d534
shxbby <148192389+shxbby@users.noreply.github.com>
2024-07-24 07:46:55 +0530

    Add files via upload

---
+++ Que_4.kt
@@ -0,0 +1,10 @@
+//4. Write a program to calculate simple interest.
+fun main() {
+    var amount = 10000
+    var rate = 10
+    var time = 2
+
+    var interest = (amount*rate*time)/100
+
+    println("Interest = $interest")
+}
