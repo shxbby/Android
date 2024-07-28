e28aff9c43223bcde206fd939cb1547104af5378
shxbby <148192389+shxbby@users.noreply.github.com>
2024-07-28 18:20:06 +0530

    Add files via upload

---
+++ Que_11.kt
@@ -0,0 +1,10 @@
+//11. Write a program to calculate area and perimeter of the rectangle
+
+fun main() {
+    var len = 10
+    var hight = 5
+
+    println("Area of Rectangle : ${len*hight}")
+    println("Area of Perimeter : ${2 *(len+hight)}")
+
+}
