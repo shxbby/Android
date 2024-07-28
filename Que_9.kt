aca7915651bbae2c3ee106bbad8dc183b29b2b42
shxbby <148192389+shxbby@users.noreply.github.com>
2024-07-28 18:19:29 +0530

    Add files via upload

---
+++ Que_9.kt
@@ -0,0 +1,10 @@
+//9. Write a program to find volume of cylinder (v = 3.14r2h )
+
+fun main() {
+    var radius : Double = 5.0
+    var hight : Double = 10.0
+    var pi : Double = 3.14
+
+    println("volume of cylinder : ${pi*radius*radius*hight}")
+
+}
