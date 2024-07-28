07424519d6373a000b6db21879ed6bde1987e56e
shxbby <148192389+shxbby@users.noreply.github.com>
2024-07-28 18:21:53 +0530

    Delete Que_10.kt

--- Que_10.kt
+++
@@ -1,8 +0,0 @@
-//10. Write a program to calculate area of triangle (a = 1/2hb)
-
-fun main() {
-    var base : Double = 5.3
-    var hight : Double = 6.0
-
-    println("Area of Triangle : ${0.5*base*hight}")
-}
