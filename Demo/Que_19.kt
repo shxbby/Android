cadd6025044f4fcafedd0c6fde550b9128256bf1
shxbby <148192389+shxbby@users.noreply.github.com>
2024-07-30 07:48:35 +0530

    Add files via upload

---
+++ Demo/Que_19.kt
@@ -0,0 +1,12 @@
+//19. Write a program to read the value of X and Y and print the result of following expression
+//(2X+3Y)/XY
+
+fun main() {
+    var x : Double= 30.0
+    var y : Double = 20.0
+    var result : Double = (2*x+3*y)/x*y
+
+    println("Value of X : $x")
+    println("Value of Y : $y")
+    println("Result : $result")
+}
