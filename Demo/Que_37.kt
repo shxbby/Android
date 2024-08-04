7fe62812e93b5286f6ed758a4bb59a731163ff3f
shxbby <148192389+shxbby@users.noreply.github.com>
2024-08-04 19:05:01 +0530

    Add files via upload

---
+++ Demo/Que_37.kt
@@ -0,0 +1,20 @@
+//37. Write a program to find out minimum from three number
+
+fun main() {
+    print("Enter Number 1 : ")
+    var n1 = readLine()!!.toInt()
+    print("Enter Number 2 : ")
+    var n2 = readLine()!!.toInt()
+    print("Enter Number 3 : ")
+    var n3 = readLine()!!.toInt()
+
+    if(n1<n2 && n1<n3){
+        println("$n1 is Minimum Number...")
+    }
+    else if(n2<n1 && n2<n3){
+        println("$n2 is Minimum Number...")
+    }
+    else{
+        println("$n3 is Minimum Number...")
+    }
+}
