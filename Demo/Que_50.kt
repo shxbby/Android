2ddaf20fd723c76adf53e48d958e1cb7abc3a57c
shxbby <148192389+shxbby@users.noreply.github.com>
2024-08-08 10:13:19 +0530

    Add files via upload

---
+++ Demo/Que_50.kt
@@ -0,0 +1,15 @@
+//50. Print 1 3 5 7 9
+
+fun main() {
+    for (i in 1..9 step 2) {
+        println(i)
+    }
+//Using While Loop
+    var i = 1
+    do {
+        println(i)
+        i += 2
+    } while (i < 10)
+}
+
+
